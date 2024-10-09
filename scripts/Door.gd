fb991ce96d9b2e34d5a050de3d51ca1384136b9c
Daniel <dada0009@student.monash.edu>
2024-10-09 11:20:09 +1100

    Fixed loading bugs caused by doors
    Doors now load scenes dynamically using the file path instead of packed scene reference.

--- scripts/Door.gd
+++ scripts/Door.gd
@@ -1,12 +1,12 @@
 extends Interactable
 class_name Door
 
-@export var target_scene: PackedScene # Scene to load when interacting with the door
+@export var target_scene_path: String # Path to the scene to transition to
 @export var target_position: Vector2 # Position to place the player in the new scene
 
 func _ready() -> void:
 	# Optional: Ensure necessary attributes are set
-	if not target_scene:
+	if not target_scene_path:
 		print("Warning: No target scene assigned to door!")
 	if not target_position:
 		print("Warning: No target position assigned to door!")
@@ -23,21 +23,25 @@ func _process(_delta: float) -> void:
 func transition():
 	print("Transitioning to new scene...")
 
-	# Check if target_scene is valid
-	if target_scene:
-		# If the target_scene is different, instantiate the new scene
-		LogoTransition.change_scene(target_scene, false, false, true, target_position)	
-	else:
-		print("Error: target_scene is null!")
-		print("Same scene detected, moving player to target position.")
+	# Load the target scene
+	var is_valid_scene = false
+	if target_scene_path:
+		var target_scene = ResourceLoader.load(target_scene_path)
+		if target_scene:
+			is_valid_scene = true
+			LogoTransition.change_scene(target_scene, false, false, true, target_position)	
 
-		# Move the player to the target position without changing the scene
-		var current_scene = get_tree().current_scene
-		var player = current_scene.get_node("player")
-		if player:
-			player.global_position = target_position
-		else:
-			print("Warning: Player not found in the current scene!")
+	# Check if target_scene is valid
+	if !is_valid_scene:
+		print("Error: target scene couldn't be loaded!")
+
+		# # Move the player to the target position without changing the scene
+		# var current_scene = get_tree().current_scene
+		# var player = current_scene.get_node("player")
+		# if player:
+		# 	player.global_position = target_position
+		# else:
+		# 	print("Warning: Player not found in the current scene!")
 	return
 
 
